3cdfd91bd8fb41ae6616b368cea7449d73421250
panda8z <panda8@126.com>
2021-01-22 23:23:19 +0800

    change editor folder

---
+++ web/v-admin/src/components/editor/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Editor :init="init"></Editor>
+  </div>
+</template>
+<script>
+
+import Editor from '@tinymce/tinymce-vue'
+import tinymce from './tinymce.min.js'
+import './icons/default/icons.min.js'
+import './themes/silver/theme.min.js'
+import './langs/zh_CN'
+import './plugins/preview/'
+export default {
+  // props: [
+  //   value: {
+
+  //   }
+  // ],
+  components: { Editor },
+  data() {
+    return {
+      init: {
+        language: 'zh_CN'
+      }
+    }
+  },
+
+}
+</script>
+<style scope>
+@import url("./skins/ui/oxide/skin.min.css");
+</style>
